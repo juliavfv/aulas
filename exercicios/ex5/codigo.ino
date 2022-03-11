e8fc3ec1a5a0acff56db32434fdf5101e6e2ce35
juliavfv <99338699+juliavfv@users.noreply.github.com>
2022-03-11 11:21:54 -0300

    Create codigo.ino

---
+++ exercicios/ex5/codigo.ino
@@ -0,0 +1,22 @@
+const int led1 = 7;
+const int botao1 = 6;
+int valor1;
+  
+void setup()
+{
+  pinMode(botao1, INPUT);
+  pinMode(led1, OUTPUT);
+}  
+
+void loop()
+{
+  valor1 = digitalRead(botao1);
+  
+  if(valor1){
+  digitalWrite(led1, LOW); 
+  }else{
+  digitalWrite(led1, HIGH);
+  }
+  delay(10);  
+
+}
